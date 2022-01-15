014fa147566646920ef1a5b6dd46699dfcc97223
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-15 13:31:49 +0100

    convert pub key to EC key

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,38 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - a function that converts a public key to an EC key.
+ * @pub: pointer to public key
+ *
+ * Return: pointer to EC key, NULL on error
+ **/
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_KEY *key;
+	EC_POINT *point;
+
+	if (!pub)
+		return (NULL);
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!key)
+		return (NULL);
+
+	point = EC_POINT_new(EC_KEY_get0_group(key));
+	if (!point)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL)
+		|| !EC_KEY_set_public_key(key, point))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
+		return (NULL);
+	}
+
+	EC_POINT_free(point);
+	return (key);
+}
