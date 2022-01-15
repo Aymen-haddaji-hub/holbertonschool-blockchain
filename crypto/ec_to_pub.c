123d17b07fd39a2895c04b2189e1298bb3a5ea55
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-15 12:44:01 +0100

    push ec_create and ec_obj to bublic key

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -9,22 +9,13 @@
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 	const EC_POINT *point;
-	const EC_GROUP *group;
-	size_t pub_key_len;
 
-	if (!key || !pub)
+	if (key == NULL)
 		return (NULL);
 
 	point = EC_KEY_get0_public_key(key);
-	group = EC_KEY_get0_group(key);
-	if (!point || !group)
-		return (NULL);
-
-	pub_key_len = EC_POINT_point2oct(group, point,
-					 POINT_CONVERSION_UNCOMPRESSED,
-					 pub, EC_PUB_LEN, NULL);
-	if (pub_key_len != EC_PUB_LEN)
-		return (NULL);
+	EC_POINT_point2oct(EC_KEY_get0_group(key), point,
+			POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL);
 
 	return (pub);
 }
