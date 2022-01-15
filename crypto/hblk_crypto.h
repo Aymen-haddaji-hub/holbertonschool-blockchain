1bc153365a3e32fc8e0f623b842422ec3db58da7
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-15 13:04:23 +0100

    fix extraction and NID definition in header

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,15 +1,27 @@
 #ifndef HBLK_CRYPTO_H
 #define HBLK_CRYPTO_H
 
-#include <stdint.h>
+
+#include <sys/stat.h>
 #include <openssl/sha.h>
 #include <openssl/ec.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
 
-#define NID_secp256k1 714
-#define EC_PUB_LEN 65
+# define EC_CURVE   NID_secp256k1
+
+/* EC_KEY public key octet string length (using 256-bit curve) */
+# define EC_PUB_LEN 65
+/* Maximum signature octet string length (using 256-bit curve) */
+# define SIG_MAX_LEN    72
+
+# define PRI_FILENAME   "key.pem"
+# define PUB_FILENAME   "key_pub.pem"
 
 
 uint8_t *sha256(int8_t const *s, size_t len,
