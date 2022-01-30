a92af317854fbd4b5206594866e1e68606439757
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-30 12:04:03 +0100

    hash match diff

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -1,34 +1,24 @@
 #include "blockchain.h"
 
 /**
- * hash_matches_difficulty - check if a hash matches the difficulty
- * @hash: hash to check
- * @difficulty: difficulty to check against
- * Return: 1 if hash matches difficulty, 0 otherwise
- **/
+ * hash_matches_difficulty - Checks if a hash matches a given difficulty
+ * @hash: Hash to be checked
+ * @difficulty: Difficulty to be checked against
+ * Return: 1 if the hash matches the difficulty, 0 otherwise
+ */
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-					uint32_t difficulty)
+							uint32_t difficulty)
 {
-	int i;
-	uint32_t diff = 0;
-	uint8_t *p = (uint8_t *)hash;
+	uint8_t const *p = hash;
+	uint32_t i;
 
-	if (!hash)
-		return (0);
-	for (; p < hash + SHA256_DIGEST_LENGTH; p++)
-	{
-		for (i = 7; i >= 0; i--)
-		{
-			if ((*p >> i) & 1)
-			{
-				if (diff >= difficulty)
-					return (1);
-				return (0);
-			}
-			diff++;
-		}
-	}
-	if (diff < difficulty)
-		return (0);
+	for (i = 0; i < difficulty / 8; i++)
+		if (*p++ != 0x00)
+			return (0);
+
+	if (difficulty % 8)
+		if ((*p & (0xFF << (8 - difficulty % 8))) != 0)
+			return (0);
+
 	return (1);
 }
