dec061edf8bc3e14b15326954c31ef02a1234b53
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-30 16:13:08 +0100

    update amount

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -16,6 +16,7 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t *pub[EC_PUB_LEN])
 	tx_out = calloc(1, sizeof(*tx_out));
 	if (!tx_out)
 		return (NULL);
+	tx_out->amount = amount;
 	memcpy(tx_out->pub, pub, sizeof(tx_out->pub));
 	if (!sha256((int8_t const *)tx_out, sizeof(tx_out->amount) +
 				sizeof(tx_out->pub), tx_out->hash))
