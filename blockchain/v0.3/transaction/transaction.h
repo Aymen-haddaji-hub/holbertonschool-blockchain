c0c3bb70cdfe2757336c1a02207bd6d244294aed
Aymen-haddaji-hub <aymensystem7@gmail.com>
2022-01-31 20:41:15 +0100

    tx validity checks

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -105,19 +105,20 @@ typedef struct Validation_Visitor
 } validation_vistor_t;
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
-unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
-			uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
+unspent_tx_out_t *unspent_tx_out_create(
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out);
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
 uint8_t *transaction_hash(
-    transaction_t const *transaction,
-    uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+		transaction_t const *transaction,
+		uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 sig_t *tx_in_sign(tx_in_t *in,
-    uint8_t const tx_id[SHA256_DIGEST_LENGTH],
-    EC_KEY const *sender, llist_t *all_unspent);
+	uint8_t const tx_id[SHA256_DIGEST_LENGTH],
+	EC_KEY const *sender, llist_t *all_unspent);
 transaction_t *transaction_create(EC_KEY const *sender,
-	EC_KEY const *receiver,
-	uint32_t amount,
-	llist_t *all_unspent);
+		EC_KEY const *receiver,
+		uint32_t amount,
+		llist_t *all_unspent);
 int transaction_is_valid(transaction_t const *transaction,
 	llist_t *all_unspent);
 #endif /* _TRANSACTION_H_ */
